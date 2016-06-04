5ce128112c83251cd9b26a70e2deaf8ae5b28a27
vcve <vcve@hotmail.com>
2016-06-05 01:57:54 +0800

    the stage 3 project

---
+++ Stage-3-Create-a-Movie-Website/entertainment_center.py
@@ -0,0 +1,32 @@
+# Lesson 3.4: Make Classes
+# Mini-Project: Movies Website
+
+# In this file, you will define instances of the class Movie defined
+# in media.py. After you follow along with Kunal, make some instances
+# of your own!
+
+# After you run this code, open the file fresh_tomatoes.html to
+# see your webpage!
+
+import media
+import fresh_tomatoes
+
+the_shawshank_redemption = media.Movie("The Shawshank Redemption",
+                                       "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                                       "https://img1.doubanio.com/lpic/s27998208.jpg",
+                                       "https://www.youtube.com/watch?v=AAzVYa3L31M" )
+leon = media.Movie("Leon",
+                   "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. Léon and Mathilda form an unusual relationship, as she becomes his protégée and learns the assassin's trade.",
+                   "https://img3.doubanio.com/view/photo/photo/public/p2168338256.jpg",
+                   "https://www.youtube.com/watch?v=OFHoSvkfyRY")
+
+
+Ba_wang_bie_ji = media.Movie("Farewell My Concubine",
+                             "The story of two men, who met as apprentices in the Peking Opera, and stayed friends for over 50 years.",
+                             "https://upload.wikimedia.org/wikipedia/zh/0/0c/Bawangbieji.jpg",
+                             "https://www.youtube.com/watch?v=npNscbT5xwI")
+
+
+movies = [the_shawshank_redemption, leon, Ba_wang_bie_ji]
+
+fresh_tomatoes.open_movies_page(movies)
